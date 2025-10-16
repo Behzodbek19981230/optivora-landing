a698f05bed33a8b97193c64a7d06824de71fe3d0
unknown <behzodrasulov432@gmail.com>
2025-10-16 21:38:52 +0500

    fix:all

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -7,6 +7,7 @@ import ReduxProvider from '@/providers/ReduxProvider'
 
 export const metadata: Metadata = {
   title: 'Optivora',
+  icons:'/logo.svg',
   description: 'Optivora is a modern trade and service company specializing in the supply and resale of industrial equipment and pumping systems.\r\n\r\nWe partner with leading global manufacturers such as Torishima, Ingersoll, KSB, and Sigma Group, providing our clients with reliable, energy-efficient, and proven solutions.\r\n\r\nOptivora offers a comprehensive approach: from equipment selection to delivery and technical support. We value the trust of our partners and strive to provide a high level of service, quality, and professionalism in all aspects of our work.',
 }
 
